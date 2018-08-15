df6d185088858f7135460814cba6d03b1d9f36ee
sriramremindly <sriram.remindly@gmail.com>
2018-08-15 16:45:51 -0500

    some new changes

---
+++ src/DataService/MenuService.ts
@@ -0,0 +1,10 @@
+import {MenuServiceInterface} from './Interfaces/IMenuService';
+import { Injectable, EventEmitter } from '../../node_modules/@angular/core';
+
+@Injectable()
+export class MenuService implements MenuServiceInterface {
+
+    getMenuItems() : any {
+
+    }
+}
--- src/pages/Products/products.ts
+++ src/pages/Products/products.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import {ListPage} from '../list/list';
 
 @Component({
   selector: 'page-products',
   templateUrl: 'products.html'
 })
 export class ProductsPage {
-
+  rootPage: any = ListPage;
   constructor(public navCtrl: NavController) {
 
   }
